1b04e92ee89f05d0fd5b1d9213407da89923f1df
azrafe7 <azrafe7@gmail.com>
2017-10-03 00:00:42 +0200

    more tests

--- test/Test.hx
+++ test/Test.hx
@@ -24,11 +24,30 @@ class Test {
   public function testInt64Range() {
     Assert.equals(MAX_INT64_STR, MAX_INT64.asString());
     Assert.equals(MIN_INT64_STR, MIN_INT64.asString());
+    
+    Assert.isTrue(Int64.make(0x7FFFFFFF, 0xFFFFFFFF) == MAX_INT64);
+    Assert.isTrue(Int64.make(0x80000000, 0x00000000) == MIN_INT64);
   }
   
   public function testFloatRange() {
     var maxFloat:Float = MAX_INT64_FLOAT;
     var minFloat:Float = MIN_INT64_FLOAT;
+    
+    Assert.equals(MAX_FLOAT_INT64_STR, maxFloat.f2wi().asString());
+    Assert.equals(MIN_FLOAT_INT64_STR, minFloat.f2wi().asString());
+    
+    Assert.raises(function ():Void {
+      floatToInt64(maxFloat + 1.);
+    });
+    
+    Assert.raises(function ():Void {
+      floatToInt64(minFloat - 1.);
+    });
+  }
+  
+  public function testFloatRangeWithPow() {
+    var maxFloat:Float = Math.pow(2., 53) - 1;
+    var minFloat:Float = Math.pow(-2., 53) + 1;
     
     Assert.equals(MAX_FLOAT_INT64_STR, maxFloat.f2wi().asString());
     Assert.equals(MIN_FLOAT_INT64_STR, minFloat.f2wi().asString());
@@ -61,7 +80,7 @@ class Test {
     Assert.isTrue(0 == floatToInt64(-0.1));
   }
   
-  public function testFromInt():Void {
+  public function testFromInt() {
     Assert.isTrue(0 == intToInt64(0));
     Assert.isTrue(0 == 0.i2wi());
     
@@ -78,6 +97,34 @@ class Test {
     fromIntHex = maxInt.i2wi();
     Assert.isTrue(maxInt == fromIntHex);
     Assert.isTrue(fromIntHex > 0);
+  }
+  
+  public function testFromNumExpr() {
+    var i64:Int64 = exprToInt64(31.312);
+    
+    Assert.isTrue(31 == i64);
+    Assert.isTrue(666 == 666.x2wi());
+    Assert.isTrue(666 == 666.5.x2wi());
+    Assert.isTrue(666 == 666.75.x2wi());
+    
+    Assert.isTrue(2147483647 == 2147483647.x2wi());
+    Assert.isTrue(-2147483647 == -2147483647.x2wi());
+    Assert.isTrue(-2147483647 == -2147483647.8.x2wi());
+  }
+  
+  public function testFromSciNotation() {
+    var i64:Int64 = exprToInt64(-0.0e0);
+    Assert.isTrue(0 == i64);
+    
+    i64 = -0.0e3.x2wi();
+    Assert.isTrue(0 == i64);
+    
+    var smallFloat:Float = -31.0e-12;
+    Assert.isTrue(0.x2wi() == smallFloat.f2wi());
+    Assert.isTrue(0.x2wi() == -31.0e-12.x2wi());
+    
+    Assert.isTrue(2e15 == 2e15.x2wi().wi2f());
+    Assert.isTrue(-2e15 == -2e15.x2wi().wi2f());
   }
 }
 
